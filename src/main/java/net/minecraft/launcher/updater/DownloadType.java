bf74e122ce3aabc8d3554b1b1711a1edf1452a10
BrightSkyz <BrightSkyz@users.noreply.github.com>
2019-09-17 22:12:57 -0400

    Fix 1.14.4+ not launching due to mappings being added, bumped version for release

--- src/main/java/net/minecraft/launcher/updater/DownloadType.java
+++ src/main/java/net/minecraft/launcher/updater/DownloadType.java
@@ -3,10 +3,11 @@ package net.minecraft.launcher.updater;
 public enum DownloadType {
     CLIENT,
     SERVER,
+    CLIENT_MAPPINGS, // Add mappings to fix 1.14.4+
+    SERVER_MAPPINGS,
     WINDOWS_SERVER;
     
 
     private DownloadType() {
     }
 }
-
